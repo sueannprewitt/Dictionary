a6b919b9fc2164f7139a59ccb914cc4fc1670e4e
Sue Ann <sueannprewitt@yahoo.com>
2017-09-12 14:08:45 -0400

    Initial commit

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Dictionary {
 	class Program {
+		void Run() {
+			Dictionary<string, string> StateCodes = new Dictionary<string, string>();
+			StateCodes["OH"] = "Ohio";
+			StateCodes["KY"] = "Kentucky";
+			StateCodes["IN"] = "Indiana";
+
+
+			
+
+			foreach(var key in StateCodes.Keys) {
+				Console.WriteLine($"The state name for code KY is {StateCodes["KY"]}");
+			}
+		}
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
